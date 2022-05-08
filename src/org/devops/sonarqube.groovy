5c55cac572631f2d1b98b3d411bfb9b71765ca89
张阳华 <zhangyanghua@pengwin.com>
2022-05-08 23:42:37 +0800

    second

--- src/org/devops/sonarqube.groovy
+++ src/org/devops/sonarqube.groovy
@@ -1,9 +1,12 @@
 package org.devops
 
 
-// 代码扫描
+// 需要先安装 jenkins 插件: SonarQube Scanner
+// 代码中涉及sonarqube api调用，因此需要先创建 jenkins 凭据，类型：username/password
+
+// 代码扫描, 接收3个参数：项目/程序名（用来查找或者创建sonarqube项目），打包类型（maven/npm等，用来映射判断语言是什么），打包工具集（里面定义了sonar-scanner工具的家目录)
 def SonarScan(projectName, projectType, buildTools){
-    //搜索项目
+    //搜索项目， 返回项目是否存在的结果
     result = SerarchProject(projectName)
     println(result)
     
@@ -15,7 +18,8 @@ def SonarScan(projectName, projectType, buildTools){
         println("${projectName}---项目已存在！")
     }
     
-    //配置项目质量规则
+    // 配置项目质量规则,从项目名里获取.
+    // 项目名规则： 质量规则-项目名
     qpName=projectName.split("-")[0]   //anyops
     //qpName="myjava"
 
@@ -31,6 +35,8 @@ def SonarScan(projectName, projectType, buildTools){
     ConfigQualityGates(projectName,qpName)
 
 
+    // 这个DSL关键词 withSonarQubeEnv 由 jenkins 插件: SonarQube Scanner 提供，用于关联 jenkins web 里定义 SonarQube 服务器地址
+    // 开始扫描
     withSonarQubeEnv("mysonarserver"){
         def sonarDate = sh  returnStdout: true, script: 'date  +%Y%m%d%H%M%S'
         sonarDate = sonarDate - "\n"
@@ -68,18 +74,21 @@ def SonarScan(projectName, projectType, buildTools){
        }   
     }
     
-
+    // 这个等待时间是必要的。因为从sonarqube获取的扫描结果可能并不是刚扫描出的结果，这个扫描出的结果是有延迟的。
     sleep 10
-    //获取扫描结果
+
+    //获取扫描结果, 如果超出质量阈，则执行 error 关键词，推出流水线。
     result = GetProjectStatus(projectName)
     println(result)
     if (result.toString() == "ERROR"){
+        mytools.EmailUser(userEmail,"代码质量阈错误")
         error " 代码质量阈错误！请及时修复！"
     } else {
         println(result)
     }
 }
 
+// 下方是附属函数
 
 def HttpReq(reqType,reqUrl,reqBody){
     def sonarServer = "http://192.168.1.200:9000/api"
@@ -112,6 +121,7 @@ def SerarchProject(projectName){
 }
 
 //获取Sonar质量阈状态
+// 这个接口从 sonarqube 项目详情页里通过F12就可以看到，是一个get请求,并返回json字符串，包含质量阈的结果
 def GetProjectStatus(projectName){
     apiUrl = "project_branches/list?project=${projectName}"
     response = HttpReq("GET",apiUrl,'')
